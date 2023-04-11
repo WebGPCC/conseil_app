a148739849ea4e4a21042a1772af0775e70ed88b
WebGPCC <webdevpcc@gmail.com>
2023-04-11 15:32:21 +0200

    searchbar search word mistake allowed

--- components/SymptomesList.tsx
+++ components/SymptomesList.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import SymptomeCard from './SymptomeCard'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 interface SymptomeListProps{
   data:Record<string,any>
@@ -8,8 +10,17 @@ interface SymptomeListProps{
 }
 
 const SymptomesList:React.FC<SymptomeListProps> = ({data, isLoading, filterby}) => {
+  console.log(data)
 
-  if(isLoading){
+  const symptomes = data.filter((element:Record<string,any>)=>element
+  .name.toLocaleLowerCase().replace(/ /g,'').includes(
+    filterby.toLocaleLowerCase().replace(/ /g,'')
+  ))
+    .map((symptome: Record<string,any>) => (
+      <SymptomeCard key={symptome.id} data={symptome}/>
+    ))
+
+  if(!data ||isLoading){
     return (
       <div className='flex justify-around  items-center text-bold text-4xl'>Loading...</div>
     )
@@ -17,10 +28,30 @@ const SymptomesList:React.FC<SymptomeListProps> = ({data, isLoading, filterby}) 
 
   return (
       <div className='flex justify-around  items-center flex-wrap gap-10 m:gap-24 m:px-36 pb-16 px-10'>
-        {data.filter((element:Record<string,any>)=>element.name.includes(filterby))
-        .map((symptome: Record<string,any>) => (
-          <SymptomeCard key={symptome.id} data={symptome}/>
-        ))}
+        { symptomes.length != 0?
+            symptomes
+          :
+            (<span className=" 
+                  flex 
+                  justify-center 
+                  items-center 
+                  text-bold 
+                  text-xl 
+                  text-gray-700 
+                  py-8 
+                  px-4 
+                  border 
+                  border-2 
+                  border-dotted
+                  rounded-xl
+              ">
+                  <span className="text-4 xl mr-5">
+                      <FontAwesomeIcon icon={faMagnifyingGlass}/>
+                  </span>
+                  Aucun produit ne correspond à la recherche
+              </span>
+            )
+        }
       </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,6 @@ export default function Home() {
   const [value, setvalue] = useState('')
   const {data: symptomes = [], isLoading} = useSymptomes()
 
-
-
   return (
     <div>
       <h1 className=" flex justify-center flex-wrap text-center font-bold text-2xl py-10  sm:pb-20 sm:text-4xl" >Une solution pour chacun de vos<span className="text-green-600">&nbsp;besoins</span></h1>
